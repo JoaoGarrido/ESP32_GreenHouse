a0f7d63bc29722815b3886ebb7d84617df8a21b8
EmanuelCastro <up201606028@fe.up.pt>
2020-05-09 18:49:27 +0100

    + Functional watchdog + DHT function

--- src/main.c
+++ src/main.c
@@ -6,17 +6,17 @@
 #include "nvs.h"
 #include "nvs_flash.h"
 #include "esp_task_wdt.h"
-//#include "dht.h"
-#include "dht11.h"
-#include "driver/gpio.h"
-#include "sdkconfig.h"
+#include "dht.h"
+//#include "dht11.h"
+//#include "driver/gpio.h"
+//#include "sdkconfig.h"
 
 //Cores
 #define WIFI_COMMUNICATIONS_CORE 0
 #define APPLICATION_CORE 1
 
 //SSID config
-#define EXAMPLE_WIFI_SSID "The Marley"
+#define EXAMPLE_WIFI_SSID "TheMarley"
 #define EXAMPLE_WIFI_PASS "7EC2A4F115"
 static EventGroupHandle_t wifi_event_group;
 const int CONNECTED_BIT = BIT0;
@@ -44,11 +44,11 @@ const int CONNECTED_BIT = BIT0;
 
 
 //DHT
-//static const dht_sensor_type_t sensor_type = DHT_TYPE_DHT11;
+static const dht_sensor_type_t sensor_type = DHT_TYPE_DHT11;
 //#if defined(CONFIG_IDF_TARGET_ESP8266)
 //static const gpio_num_t dht_gpio = 4;
 //#else
-//static const gpio_num_t dht_gpio = 4;
+static const gpio_num_t dht_gpio = 4;
 //#endif
 
 
@@ -76,7 +76,7 @@ void initialize_nvs(){
 }
 
 void initialize_ports(){
-   DHT11_init(GPIO_NUM_4);
+   //DHT11_init(GPIO_NUM_4);
 }
 
 void initialize_wifi_sta_mode(){
@@ -101,22 +101,25 @@ void initialize_wifi_sta_mode(){
 
 //Loop task functions
 void read_DHT22(void *args){
-
     //Subscribe this task to TWDT, then check if it is subscribed
     CHECK_ERROR_CODE(esp_task_wdt_add(NULL), ESP_OK);
     CHECK_ERROR_CODE(esp_task_wdt_status(NULL), ESP_OK);
 
     for(;;){
-        CHECK_ERROR_CODE(esp_task_wdt_reset(), ESP_OK);  //Comment this line to trigger a TWDT timeout
-        float humidity = 0;
-        float temperature = 0;
-        //dht_read_float_data(sensor_type, dht_gpio, &humidity, &temperature);
-        humidity = DHT11_read().humidity;
-        temperature = DHT11_read().temperature;
-        ESP_LOGI(task_logging,"Task running: %s", "read_DHT22");
-        ESP_LOGI(dht22_tag,"Temperature: %f || Humidity %f", temperature, humidity);
+        
+          //Comment this line to trigger a TWDT timeout
+        float humidity = 0.0;
+        float  temperature = 0.0;
+        if (dht_read_float_data(sensor_type, dht_gpio, &humidity, &temperature) == ESP_OK){
+            CHECK_ERROR_CODE(esp_task_wdt_reset(), ESP_OK);
+            ESP_LOGI(task_logging,"Task running: %s", "read_DHT22");
+            ESP_LOGI(dht22_tag,"Temperature: %fºC || Humidity %f%%", temperature, humidity);
+        }
+        //humidity = DHT11_read().humidity;
+        //temperature = DHT11_read().temperature;
+        
 
-        vTaskDelay(1000 / portTICK_RATE_MS);
+        vTaskDelay(2000 / portTICK_RATE_MS);
     }  
 }
 
@@ -184,7 +187,7 @@ void app_main(){
     initialize_ports();
 
     //Application Tasks  
-    xTaskCreatePinnedToCore(read_DHT22, "read_DHT22", TASK_STACK_MIN_SIZE*3, NULL, 5, NULL, APPLICATION_CORE);
+    xTaskCreatePinnedToCore(read_DHT22, "read_DHT22", TASK_STACK_MIN_SIZE, NULL, 5, NULL, APPLICATION_CORE);
     //xTaskCreatePinnedToCore(update_motor_status, "update_motor_status", TASK_STACK_MIN_SIZE, NULL, 4, NULL, APPLICATION_CORE);
     //xTaskCreatePinnedToCore(control_greenhouse, "control_greenhouse", TASK_STACK_MIN_SIZE, NULL, 3, NULL, APPLICATION_CORE);
     //xTaskCreatePinnedToCore(write_display, "write_display", TASK_STACK_MIN_SIZE, NULL, 2, NULL, APPLICATION_CORE);
